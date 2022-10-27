59eb90ebe9f8911a628ba9ae056e430415b8762b
Dimitar <dimitar45minchev@gmail.com>
2022-10-27 13:52:24 +0300

    Add project files.

---
+++ NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+
+        public override double FuelConsumption { get => 8; set => base.FuelConsumption = value; }
+    }
+}
---
+++ NeedForSpeed/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public double DefaultFuelConsumption { get { return FuelConsumption; } }
+
+        public virtual double FuelConsumption { get { return 1.25; } set { } }
+
+        public double Fuel { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            Fuel -= kilometers * (DefaultFuelConsumption);
+        }
+    }
+}
